b6d138ebf6378b78b33bb32cffa19c1b6c3158f7
LeonardoZambrano <leonardoz.bta@gmail.com>
2016-07-12 10:12:48 -0500

    ORM de Primera Propuesta de BD

---
+++ data/ReleaseOne/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	_ "ReleaseOne/docs"
+	_ "ReleaseOne/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "db_user:123Abcd#@tcp(127.0.0.1:3306)/ReleaseOne")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
