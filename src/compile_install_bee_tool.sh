dd7519d34857ed220ec84258c17e2777c525545a
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-07-18 22:39:16 -0500

    compilación auto bee tool y mejora proxy_ud

--- src/compile_install_bee_tool.sh
+++ src/compile_install_bee_tool.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-sudo su vagrant -c "
+
+repobee="github.com/beego/bee"
+
+archivos=( 
+"g_appcode.go"
+)
+
+dependencias=(
+"github.com/go-sql-driver/mysql"
+"github.com/lib/pq"
+"github.com/astaxie/beego"
+)
+
+mkdir /tmp/bee
+for i in "${archivos[@]}"; do
+  mv /home/vagrant/src/$repobee/$i /tmp/bee/$i
+done
+
+rm -rf /home/vagrant/src/github.com
+for i in "${dependencias[@]}"; do
+  rm -rf /home/vagrant/src/$i
+  go get $i
+done
+
+rm -rf /home/vagrant/src/$repobee
+go get $repobee
+
 cd /home/vagrant/src/github.com/beego/bee/
+for i in "${archivos[@]}"; do
+  cp -f /tmp/bee/$i $i
+done
 go build
-"
+
 sudo cp /home/vagrant/src/github.com/beego/bee/bee /usr/lib/golang/bin/
