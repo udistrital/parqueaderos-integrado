e13844e86bd2f5f7ac0ca61d6d727424e082cd17
LeonardoZambrano <leonardoz.bta@gmail.com>
2016-07-29 20:03:15 -0500

    Se habilita autocreacion del esquema de base de datos

--- src/ReleaseTwo/main.go
+++ src/ReleaseTwo/main.go
@@ -6,17 +6,26 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
 )
 
 func init() {
 	orm.RegisterDataBase("default", "postgres", "postgres://olzambranop:123Abcd@127.0.0.1:5432/releasetwo?sslmode=disable")
+	orm.RunCommand()
 }
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+		beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+			AllowAllOrigins:  true,
+			AllowMethods:     []string{"PUT", "PATCH", "DELETE"},
+			AllowHeaders:     []string{"Content-Type", "Origin"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+		}))
 	}
 	beego.Run()
 }
