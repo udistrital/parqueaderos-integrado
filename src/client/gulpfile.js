15ef8902193e2ef14b271c36e60b8c719d0f4ed5
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-07 00:55:03 -0500

    Se agrega web-server en la ejecución del comando $ gulp

--- src/client/gulpfile.js
+++ src/client/gulpfile.js
@@ -1,6 +1,20 @@
 //'use strict';
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
+//https://www.npmjs.com/package/gulp-webserver
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 9000,
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+//https://github.com/andresvia/go-angular-drone/blob/master/.drone.yml
+gulp.task('default', ['webserver'], function() {
   // place code for your default task here
 });
