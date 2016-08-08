a23029c982956441deeec2446ca643a89ae74a8c
Leonardo Zambrano <leonardoz.bta@gmail.com>
2016-08-08 10:06:41 -0500

    Se agrega codigo fuente y binario principal del proyecto v1 (el main del proyecto)

---
+++ src/Circe/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	_ "Circe/docs"
+	_ "Circe/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://usercirce:oascirce@localhost:5432/circe?sslmode=disable")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
