e7e7554c814e76f98038777a7bc1bee50e25e182
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-07 20:06:51 -0500

    Se comenta al estilo JSDoc

--- src/client/gulpfile.js
+++ src/client/gulpfile.js
@@ -1,8 +1,11 @@
-//'use strict';
+/**
+ * Comentanto funciones usando estilo JSDoc http://usejsdoc.org/
+ * gulp - Automatizador flujo de trabajo en el front-End
+ * webserver -  https://www.npmjs.com/package/gulp-webserver
+ */
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
-//https://www.npmjs.com/package/gulp-webserver
 gulp.task('webserver', function() {
   gulp.src('app')
     .pipe(webserver({
@@ -13,6 +16,13 @@ gulp.task('webserver', function() {
       open: true
     }));
 });
+
+/*
+//https://www.youtube.com/watch?v=TGiBG6II6Jk
+gulp.task('watch', ['sass'], function (){
+  gulp.watch('./sass/**\/*.css', ['sass'])
+});
+*/
 
 //https://github.com/andresvia/go-angular-drone/blob/master/.drone.yml
 gulp.task('default', ['webserver'], function() {
