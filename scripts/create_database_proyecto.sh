798e42a4b25ea4eafa951d192c67065ccdd07eab
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-08-17 22:35:34 -0500

    Se corrigen scripts

--- scripts/create_database_proyecto.sh
+++ scripts/create_database_proyecto.sh
@@ -3,14 +3,9 @@ echo 'Ejecutando: create_database_proyecto.sh'
 sql1=usercirce.sql
 sql2=script_tables.sql
 
-if echo '\connect circe;' | sudo psql -U postgres&>/dev/null;then
+if echo '\connect circe; SELECT * FROM public.grupo_isla;' | sudo psql -U postgres&>/dev/null;then
   echo 'La base de datos ya está creada. Nada que hacer.'
 else
-  if echo '\connect circe; SELECT * FROM public.grupo_isla;' | sudo psql -U postgres&>/dev/null;then
-    echo 'La base de datos ya está creada. Nada que hacer.'
-  else
-
-mv $sql1 /tmp
 mv $sql2 /tmp
 scriptsql1=/tmp/$sql1
 scriptsql2=/tmp/$sql2
@@ -21,5 +16,4 @@ cd /tmp
 psql -f $scriptsql1
 psql -f $scriptsql2 -d circe
 "
-  fi
 fi
