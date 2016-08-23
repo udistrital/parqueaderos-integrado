da5e65229494481327aeea7c1fc11f17e0e224d9
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-22 23:53:38 -0500

    Creando workspace parqueaderos

--- src/Visor/scripts/createWorkspace.go
+++ src/Visor/scripts/createWorkspace.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 import "net/http"
 import "io/ioutil"
+import "bytes"
 
 func main() {
     var username string = "admin"
     var passwd string = "geoserver"
     var url string = "http://192.168.33.10:8080/geoserver/rest/"
     client := &http.Client{}
-    req, err := http.NewRequest("GET", url + "workspaces", nil)
+    var jsondata string = "{'workspace':{'name':'parqueaderos'}}"
+    b := bytes.NewBuffer([]byte(jsondata))
+    req, err := http.NewRequest("POST", url + "workspaces.json", b)
+    req.Header.Set("Content-Type", "application/json")
     req.SetBasicAuth(username, passwd)
     resp, err := client.Do(req)
     if err != nil {
