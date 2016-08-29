49cfb2958f411831043ceef4fa9b23dbad7151e5
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-28 23:00:00 -0500

    archivo de configuracion para conexion de base de datos

--- src/Parqueaderos/main.go
+++ src/Parqueaderos/main.go
@@ -6,10 +6,40 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"log"
 )
 
+type Conexion struct {
+	database_host string
+	database_port	string
+	database_name	string
+	database_user	string
+	database_password	string
+}
+
 func init() {
-	orm.RegisterDataBase("default", "postgres", "postgres://usercirce:oascirce@localhost:5432/circe?sslmode=disable")
+	data, err := ioutil.ReadFile("conexion.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := make(map[interface{}]string)
+
+	err = yaml.Unmarshal([]byte(data), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	var database_host string = m["database_host"]
+	var database_port string = m["database_port"]
+	var database_name string = m["database_name"]
+	var database_user string = m["database_user"]
+	var database_password string = m["database_password"]
+
+	orm.RegisterDataBase("default", "postgres",
+		"postgres://"+database_user+":"+database_password+"@"+database_host+":"+database_port+"/"+database_name+"?sslmode=disable")
 }
 
 func main() {
