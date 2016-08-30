75f0a79dd315d1bdb54fc9b954868c8bcb2aef85
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-30 02:42:36 -0500

    Corrigiendo formato con $ gofmt -w .

--- src/Parqueaderos/conf/parameters.go
+++ src/Parqueaderos/conf/parameters.go
@@ -1,7 +1,7 @@
 package conf
 
 import (
-  "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 )
@@ -12,22 +12,22 @@ type _Parameters struct {
 	DATABASE_NAME     string
 	DATABASE_USER     string
 	DATABASE_PASSWORD string
-  BEEGO_NAMESPACE   string
+	BEEGO_NAMESPACE   string
 }
 
 var (
-  Parameters = _Parameters{}
+	Parameters = _Parameters{}
 )
 
-func init () {
-  data, err := ioutil.ReadFile("parameters.yml")
-  if err != nil {
-    log.Fatal(err)
-  }
+func init() {
+	data, err := ioutil.ReadFile("parameters.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  err = yaml.Unmarshal([]byte(data), &Parameters)
-  if err != nil {
-    log.Fatalf("error: %v", err)
-  }
-  //log.Printf("parameters:\n%v\n\n", p)
+	err = yaml.Unmarshal([]byte(data), &Parameters)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	//log.Printf("parameters:\n%v\n\n", p)
 }
--- src/Parqueaderos/routers/commentsRouter________________home_vagrant_src_Parqueaderos_controllers.go
+++ src/Parqueaderos/routers/commentsRouter________________home_vagrant_src_Parqueaderos_controllers.go
@@ -8,177 +8,177 @@ func init() {
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetOne",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetAll",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:GrupoIslaController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetOne",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetAll",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:IslaController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetOne",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetAll",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:PropietarioController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetOne",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetAll",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:TipoPropietarioController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"],
 		beego.ControllerComments{
-			Method: "GetOne",
-			Router: `/:id`,
+			Method:           "GetOne",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"],
 		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
+			Method:           "GetAll",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"],
 		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:id`,
+			Method:           "Put",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"put"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"] = append(beego.GlobalControllerRouter["Parqueaderos/controllers:VehiculoController"],
 		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:id`,
+			Method:           "Delete",
+			Router:           `/:id`,
 			AllowHTTPMethods: []string{"delete"},
-			Params: nil})
+			Params:           nil})
 
 }
