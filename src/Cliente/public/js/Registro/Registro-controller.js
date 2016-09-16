4f1db176816f5a13f90d9106f9230b21130c81ca
Leonardo Zambrano <leonardoz.bta@gmail.com>
2016-09-15 19:51:16 -0500

    Se corrige el controlador de registro en el frontend

--- src/Cliente/public/js/Registro/Registro-controller.js
+++ src/Cliente/public/js/Registro/Registro-controller.js
@@ -12,12 +12,16 @@ angular.module('myapp')
       };
 
       $scope.update = function (id) {
-        $scope.Registro = Registro.get({id: id});
+        $scope.Registro = Registro.get({
+		id: id
+	});
         $scope.open(id);
       };
 
       $scope.delete = function (id) {
-        Registro.delete({id: id},
+        Registro.delete({
+		id: id
+	  },
           function () {
             $scope.Registros = Registro.query();
           });
@@ -72,15 +76,15 @@ angular.module('myapp')
 
       $scope.clear = function () {
         $scope.Registro = {
-          
+
           "HoraEntrada": "",
-          
+
           "HoraSalida": "",
-          
+
           "IdIsla": "",
-          
+
           "IdVehiculo": "",
-          
+
           "id": ""
         };
       };
@@ -102,20 +106,45 @@ angular.module('myapp')
         });
       };
     }])
-  .controller('RegistroSaveController', ['$scope', '$uibModalInstance', 'Registro',
-    function ($scope, $modalInstance, Registro) {
+  .controller('RegistroSaveController', ['$scope', '$http', '$uibModalInstance', 'Registro', 'CONFIG',
+    function ($scope, $http, $modalInstance, Registro, CONFIG) {
       $scope.Registro = Registro;
-
+     
+      var f = [{}];
+      $http.get(CONFIG.WS_URL + '/vehiculo')
+        .success(function(data) {
+          data.forEach(function(entry, index) {
+            f[index] ={
+              Id: entry.Id,
+              IdNfc: entry.IdNfc
+      };
+          });
+          console.log(f);
+          $scope.RegistroVehIds = f;
+        })
+        .error(function(err) {});
       
+      var f1 = [];
+      $http.get(CONFIG.WS_URL + '/isla')
+        .success(function(data) {
+          data.forEach(function(entry, index) {
+            f1[index] = entry.Id;
+          });
+          console.log(f1);
+          $scope.RegistroIslIds = f1;
+        })
+        .error(function(err) {});
+
+
       $scope.HoraEntradaDateOptions = {
         dateFormat: 'yy-mm-dd',
-        
-        
+
+
       };
       $scope.HoraSalidaDateOptions = {
         dateFormat: 'yy-mm-dd',
-        
-        
+
+
       };
 
       $scope.ok = function () {
