e70e61939fb3c4c5fe9aa825672fd1882258871a
Leonardo Zambrano <leonardoz.bta@gmail.com>
2016-09-27 23:40:06 -0500

    Se agrega la directiva CONFIG la cual posee el prefijo de la ruta del backend para hacer las peticiones GET y POST para el Login

--- src/Cliente/public/js/Login/Login-controller.js
+++ src/Cliente/public/js/Login/Login-controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('myapp')
-  .controller('LoginController', ['$http', '$scope', '$uibModal',
-    function($http, $scope, $modal) {
+  .controller('LoginController', ['$http', '$scope', '$uibModal', 'CONFIG',
+    function($http, $scope, $modal, CONFIG) {
       $scope.Login = ["admin", "admin"];
 
-      var completeUrl = "http://localhost:8080/v1/login?" + "username="+ $scope.Login[0]
+      var completeUrl = CONFIG.WS_URL + '/login ' + '?' + "username=" + $scope.Login[0]
                     + "&password=" + $scope.Login[1] //and the same for input 3
 $http({method: 'GET', url: completeUrl}).
 success(function(data, status, headers, config) {
    // here data contains all informations returned by the server
    console.log(data);
+   x = document.cookie;
+   console.log(x);
 }).
 error(function(data, status, headers, config) {
    //In case your server respond with a 4XX or 5XX error code
