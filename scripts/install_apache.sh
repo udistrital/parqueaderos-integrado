743895dd9486aef061a1368f6c0fb1f257cc6b1d
Jorge Ulises <naturalmentejorge@gmail.com>
2016-10-31 09:24:46 -0500

    Update install_apache.sh

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+if httpd -v &> /dev/null
+then
+  echo 'HTTPD ya está instalado, nada que hacer.'
+else
 sudo yum install -y httpd
 sudo systemctl enable httpd
-sudo tee /etc/httpd/conf.d/permisos.conf << 'EOF'
+fi
+
+file=/etc/httpd/conf.d/permisos.conf
+if [ -f $file ]
+then
+  echo "El archivo $file ya existe. Nada que hacer."
+else
+sudo tee  << 'EOF'
 User vagrant
 Group vagrant
 EOF
-sudo tee /etc/httpd/conf.d/visor_parqueaderos.conf << 'EOF'
+fi
+
+file=/etc/httpd/conf.d/visor_parqueaderos.conf
+if [ -f $file ]
+then
+  echo "El archivo $file ya existe. Nada que hacer."
+else
+sudo tee $file << 'EOF'
 Alias "/visor" "/home/vagrant/src/Visor/app/"
 <Directory "/home/vagrant/src/Visor/app/">
     Require all granted
@@ -13,11 +31,20 @@ Alias "/visor" "/home/vagrant/src/Visor/app/"
 ProxyPass /geoserver http://localhost:8000/geoserver
 #ProxyPassReverse /geoserver http://localhost:8000/geoserver
 EOF
-sudo tee /etc/httpd/conf.d/cliente_parqueaderos.conf << 'EOF'
+fi
+
+file=/etc/httpd/conf.d/cliente_parqueaderos.conf
+if [ -f $file ]
+then
+  echo "El archivo $file ya existe. Nada que hacer."
+else
+sudo tee $file << 'EOF'
 Alias "/parqueaderos" "/home/vagrant/src/Cliente/public"
 <Directory "/home/vagrant/src/Cliente/public">
     Require all granted
 </Directory>
 ProxyPass /parqueaderos/v1 http://localhost:8080/parqueaderos/v1
 EOF
+fi
+
 sudo systemctl start httpd
