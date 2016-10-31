957d9a0bdd797fbe40d9e56b0afad7323f095c39
Jorge Ulises <naturalmentejorge@gmail.com>
2016-10-31 00:19:48 -0500

    Update install_postgresql_postgis.sh

--- scripts/install_postgresql_postgis.sh
+++ scripts/install_postgresql_postgis.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 echo 'Ejecutando: install_postgresql_postgis.sh'
-if [ -f /usr/pgsql-9.4/bin/pg_ctl ]; then
+if [ -f /usr/pgsql-9.5/bin/pg_ctl ]; then
   echo 'Postgres ya está instalado. Nada que hacer.'
 else
 echo "Instalando PostgreSQL"
-sudo yum localinstall -y http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum install -y postgresql94-server
-sudo /usr/pgsql-9.4/bin/postgresql94-setup initdb
-sudo systemctl enable postgresql-9.4
-sudo sed -i.bak 's/peer/trust/; s/ident/md5/' /var/lib/pgsql/9.4/data/pg_hba.conf
-sudo tee -a /var/lib/pgsql/9.4/data/pg_hba.conf << 'EOF'
+sudo yum localinstall -y http://yum.postgresql.org/9.5/redhat/rhel-6-x86_64/pgdg-centos95-9.5-1.noarch.rpm
+sudo yum install -y postgresql95-server
+sudo /usr/pgsql-9.5/bin/postgresql95-setup initdb
+sudo systemctl enable postgresql-9.5
+sudo sed -i.bak 's/peer/trust/; s/ident/md5/' /var/lib/pgsql/9.5/data/pg_hba.conf
+sudo tee -a /var/lib/pgsql/9.5/data/pg_hba.conf << 'EOF'
 host    all             all             0.0.0.0/0               md5
 EOF
-sudo sed -i.bak \"s/#listen_addresses = 'localhost'/listen_addresses = '*'/\" /var/lib/pgsql/9.4/data/postgresql.conf
-sudo systemctl start postgresql-9.4.service
+sudo sed -i.bak \"s/#listen_addresses = 'localhost'/listen_addresses = '*'/\" /var/lib/pgsql/9.5/data/postgresql.conf
+sudo systemctl start postgresql-9.5.service
 echo "Se ha terminado la instalación de Postgresql"
-#if you what remove # yum erase postgresql94*
+#if you what remove # yum erase postgresql95*
 cat << 'EOF'
 # ¿Cómo crear un usuario y una base de datos?
 $ sudo su postgres
 $ psql
 > CREATE USER nombre_usuario WITH PASSWORD 'clave';
 > CREATE DATABASE nombre_db WITH OWNER = nombre_usuario ENCODING = 'UTF8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
-$ vim /var/lib/pgsql/9.4/data/pg_hba.conf
+$ vim /var/lib/pgsql/9.5/data/pg_hba.conf
 #host   DATABASE        USER            ADDRESS                 METHOD
 local   all             all                                     trust
 host    all             all             127.0.0.1/32            md5
 host    all             all             localhost               md5
 host    all             all             0.0.0.0/0               md5
 host    all             all             ::1/128                 md5#sara
-$ sudo systemctl restart postgresql-9.4.service
+$ sudo systemctl restart postgresql-9.5.service
 EOF
 echo "Instalando Postgis"
 sudo yum install -y epel-release
-sudo yum install -y postgis2_94
+sudo yum install -y postgis2_95
 echo "Postgis Instalado"
 cat << 'EOF'
 # ¿Cómo instalar postgis en tu base de datos?
 $ sudo su postgres
-$ /usr/pgsql-9.4/bin/psql -p 5432
+$ /usr/pgsql-9.5/bin/psql -p 5432
 > CREATE DATABASE gistest;
 > \connect gistest;
 > CREATE EXTENSION postgis;
